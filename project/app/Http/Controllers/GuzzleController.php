897719f17099a3e307fdb02499b84d3dc862baf5
Katriena Banasik <katriena.0107@hotmail.com>
2018-12-06 15:12:44 +0100

    created hmac handler function

--- project/app/Http/Controllers/GuzzleController.php
+++ project/app/Http/Controllers/GuzzleController.php
@@ -18,8 +18,34 @@ class GuzzleController extends Controller
         $this->api_secret = env('API_SECRET');
     }
 
+    protected function hmacHandler() {
+    	$key = $this->api_key;
+    	$req_method = 'get';
+    	$path_query = 'https://api.combell.com/v2/accounts';
+    	$timestamp = time();
+    	$nonce = substr(md5(uniqid(mt_rand(), true)), 0, 8);
+    	$content = '';
+
+        $valueToSign = $this->api_key
+            . $req_method
+            . urlencode($path_query)
+            . $timestamp
+            . $nonce
+            . $content;
+
+        $signedValue = hash_hmac('sha256', $valueToSign, $this->api_secret, true);
+
+        $signature = base64_encode($signedValue);
+
+        return sprintf('hmac %s:%s:%s:%s', $this->api_key, $signature, $nonce, $timestamp);
+    }
+    
     public function index() {
+
     	require base_path() . '\vendor\autoload.php';
+
+    	$hmac = $this->hmacHandler();
+    	dd($hmac);
 
         $client = new CombellClient (
 		    [
