2448193ab5b58699ebfca0cb13accffe001f7308
daankdg <daan.boschmans@student.kdg.be>
2018-12-10 11:44:48 +0100

    api auth works now

--- project/app/Http/Controllers/GuzzleController.php
+++ project/app/Http/Controllers/GuzzleController.php
@@ -7,6 +7,7 @@ use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Client as GuzzleClient;
 use App\CombellClient;
 use GuzzleHttp\Client;
+
 class GuzzleController extends Controller
 {
 	protected $api_key;
@@ -21,50 +22,30 @@ class GuzzleController extends Controller
     protected function hmacHandler() {
     	$key = $this->api_key;
     	$req_method = 'get';
-    	$path_query = 'https://api.combell.com/';
+    	$path_query = '/v2/accounts';
     	$timestamp = time();
-    	$nonce = substr(md5(uniqid(mt_rand(), true)), 0, 8);
+    	$nonce = rand(100000, 900000);
     	$content = '';
 
-        $valueToSign = $this->api_key
-            . $req_method
-            . urlencode($path_query)
-            . $timestamp
-            . $nonce
-            . $content;
-
-        $signedValue = hash_hmac('sha256', $valueToSign, $this->api_secret, true);
-
-        $signature = base64_encode($signedValue);
-
-        return sprintf('hmac %s:%s:%s:%s', $this->api_key, $signature, $nonce, $timestamp);
+        $valueToSign = $this->api_key . $req_method . urlencode($path_query) . $timestamp . $nonce . $content;
+	
+		$signedValue = hash_hmac('sha256', $valueToSign, $this->api_secret, true);
+			
+		$signature = base64_encode($signedValue);
+			
+		return sprintf('hmac %s:%s:%s:%s', $this->api_key, $signature, $nonce, $timestamp);
+        
     }
     
     public function index() {
-
-    	require base_path() . '\vendor\autoload.php';
-
-    	// $hmac = $this->hmacHandler();
-		// dd($hmac);
-		dd($this->getTestData());
-
-        $client = new CombellClient (
-		    [
-		        'debug' => true,
-		        'base_uri' => 'https://api.combell.com',
-		        'combell_api_key' => $this->api_key,
-		        'combell_api_secret' => $this->api_secret
-		    ]
-		);
-
-		// Get detail of a hosting account
 	
-        return view('index');
+		$hmac = $this->hmacHandler();
+		dd($this->getTestData($hmac));
 	}
-    public function getTestData() {
+    public function getTestData($hmac) {
         $client = new Client();
         $uri = 'https://api.combell.com/v2/accounts';
-        $header = ['headers' => ['Authorization' => $this->hmacHandler()]];
+        $header = ['headers' => ['Authorization' => $hmac]];
         $res = $client->get($uri, $header);
         return json_decode($res->getBody()->getContents(), true);
 	}
